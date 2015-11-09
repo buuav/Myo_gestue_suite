22f2045fc55c008d6907235e6f85cad8d0171502
aamodh <aamodh@bu.edu>
2015-11-09 15:20:48 -0500

    Initial commit

---
+++ TCP_NodeJS/server_test_nodejs.m
@@ -0,0 +1,19 @@
+left=0;
+right=0;
+up=0;
+down=0;
+mystate=1;
+i=1;
+%data = num2str([up,down,left,right,mystate]);
+s = whos('data');
+tcpipServer = tcpip('0.0.0.0',55000,'NetworkRole','Server');
+ set(tcpipServer,'OutputBufferSize',s.bytes);
+  fopen(tcpipServer);
+while(i<100)
+  data = num2str([up,down,left,right,mystate+i]);
+    fwrite(tcpipServer,data,'char');
+    fwrite(tcpipServer,'\r\n');
+  pause(0.01);
+  i=i+1;
+end
+  fclose(tcpipServer);
---
+++ TCP_NodeJS/tcp[Conflict].m
@@ -0,0 +1,23 @@
+clc;
+clear;
+t = tcpip('192.168.10.15',55000,'NetworkRole','client');
+set(t, 'InputBufferSize', 5000);
+fopen(t);
+i = 1;
+time=clock;
+while etime(clock,time)<100
+    if t.BytesAvailable
+        %data(i,:) = strsplit(fread(t),'\r\n');
+        %data(i,:) = fscanf(t, '%d,%d,%d,%d,%d,%d,%d,%d\r\n')';
+        data(i,:) = char(fread(t,5,'char'));
+        %pause(0.1);
+        i = i + 1
+    end
+end
+etime(clock,time)
+ [a,b]=size(data);
+for i=1:b
+    subplot(4,2,i);
+    plot(data(:,i));
+end
+fclose(t);
