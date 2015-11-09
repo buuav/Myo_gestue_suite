e824d3e7075b8897550c261175ccbd11d782a2bc
aamodh <aamodh@bu.edu>
2015-11-09 16:36:23 -0500

    Fist and spread working

--- Matlab Code/liveMYO.m
+++ Matlab Code/liveMYO.m
@@ -9,9 +9,9 @@ time=clock;
 fs=200;
 wsize=fs/2;
 wind = 1;
-[b,a] = butter(6,0.6);
-filt_data=filter(b,a,data);
-while etime(clock,time)<40
+[b,a] = butter(6,0.5);
+
+while etime(clock,time)<20
     if t.BytesAvailable
         %data(i,:) = strsplit(fread(t),'\r\n');
         data(i,:) = fscanf(t, '%d,%d,%d,%d,%d,%d,%d,%d\r\n')';
@@ -19,13 +19,16 @@ while etime(clock,time)<40
         if ~mod(m,wsize)
             elec3raw=data(m-wsize+1:m,3);
             elec5raw=data(m-wsize+1:m,5);
-            [~,locs_Rwave3] = findpeaks(elec3raw,'MinPeakHeight',70,'MinPeakDistance',(fs/2)-2);
-            [~,locs_Rwave5] = findpeaks(elec5raw,'MinPeakHeight',70,'MinPeakDistance',(fs/2)-2);
+            filt_data=filter(b,a,elec5raw);
+            [~,locs_Rwave3] = findpeaks(elec3raw,'MinPeakHeight',60,'MinPeakDistance',(fs/2)-2);
+            [~,locs_Rwave5] = findpeaks(elec5raw,'MinPeakHeight',90,'MinPeakDistance',(fs/2)-2);
             
             if any(locs_Rwave3) && any(locs_Rwave5)
                 spread(wind) = any(locs_Rwave5);
+                fprintf('spread=%d \n',spread(wind))
             elseif any(locs_Rwave3)
-                fist(wind) = any(locs_Rwave3)
+                fist(wind) = any(locs_Rwave3);
+                fprintf('fist=%d \n',fist(wind))
             end
 
             xaxis=m-wsize+1:m;
--- Matlab Code/subplot1.m
+++ Matlab Code/subplot1.m
@@ -15,6 +15,24 @@ for i=1:b
     line([3*j 3*j], [-200 200]);
     end
 end
+
+figure
+suptitle('filtered Data')
+[a,b]=size(data);
+[c,d]=butter(9,0.5);
+filt_data=filter(c,d,data);
+psize=3;
+xaxis=1/200:1/200:a/200;
+nlines=round(a/600);
+for i=1:b
+    subplot(4,2,i);
+    plot(xaxis,filt_data(:,i));
+    %title('hi %i',i);
+    for j=1:nlines
+    line([3*j 3*j], [-200 200]);
+    end
+end
+
 figure
 suptitle('spectrogram Data')
 for i=1:b
